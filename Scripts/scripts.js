e1ff62f53b03545700f8a91300cc2bbfd1a65ebb
IsaqueMiclos <isaque15miclos@gmail.com>
2023-09-24 18:15:13 -0300

    version beta

---
+++ Scripts/scripts.js
@@ -0,0 +1,55 @@
+//Create a new task
+
+function makeAnewTask() {
+    let display = document.getElementById('floating-pop-up')
+    display.style.display = 'block'
+}
+
+let ids = 0
+let counterOfTasks = 0
+
+function createNewTask() {
+    counterOfTasks++
+    ids++
+    let display = document.getElementById('floating-pop-up')
+    let task = document.getElementById('task-title')
+    let principalTable = document.getElementById('table')
+    
+    if (counterOfTasks == 1) {
+        principalTable.innerHTML = `
+            <p id="tasks" id="${ids}">
+                <span>${ids}</span>
+                <input type="checkbox" name="task0${ids}">
+                <span>${task.value}</span>
+            </p>
+        `
+    } else {
+        principalTable.innerHTML += `
+            <p id="tasks" id="${ids}">
+                <span>${ids}</span>
+                <input type="checkbox" name="task0${ids}">
+                <span>${task.value}</span>
+            </p>
+        `
+    }
+
+    display.style.display = 'none'
+}
+
+//Cancel the creation of taks
+
+function cancelTheCreation() {
+    let display = document.getElementById('floating-pop-up')
+    display.style.display = 'none'
+}
+
+//Clear all existents tasks
+
+function clearTasks() {
+    if (counterOfTasks >= 1) {
+        let principalTable = document.getElementById('table')
+        principalTable.innerHTML = 'Cleaned, Try to create a new task'
+        ids = 0
+        counterOfTasks = 0
+    }
+}
